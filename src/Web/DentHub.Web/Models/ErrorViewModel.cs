83029d66fcb2c1ed3979f3252bbe4e98796e0f6d
Milen Nikolov <milennikolov_bg@yahoo.com>
2018-12-09 14:18:52 +0200

    Created a default ASP.NET core MVC application.

---
+++ src/Web/DentHub.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DentHub.Web.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
