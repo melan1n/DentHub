a7b8673b41542ea46ebe550a2a197eb740cd5cec
Milen Nikolov <milennikolov_bg@yahoo.com>
2018-12-18 23:59:43 +0200

    Added dropdown lists to Register Dentist form. Added cards to About View.

--- src/Web/DentHub.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Web/DentHub.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -4,8 +4,10 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using DentHub.Data;
 using DentHub.Data.Common;
 using DentHub.Data.Models;
+using DentHub.Web.Models;
 using Microsoft.AspNetCore.Authorization;
 
 using Microsoft.AspNetCore.Identity;
@@ -16,39 +18,39 @@ using Microsoft.Extensions.Logging;
 
 namespace DentHub.Web.Areas.Identity.Pages.Account
 {
-    [AllowAnonymous]
-    public class RegisterModel : PageModel
-    {
-        private readonly SignInManager<DentHubUser> _signInManager;
-        private readonly UserManager<DentHubUser> _userManager;
-        private readonly ILogger<RegisterModel> _logger;
-        private readonly IEmailSender _emailSender;
+	[AllowAnonymous]
+	public class RegisterModel : PageModel
+	{
+		private readonly SignInManager<DentHubUser> _signInManager;
+		private readonly UserManager<DentHubUser> _userManager;
+		private readonly ILogger<RegisterModel> _logger;
+		private readonly IEmailSender _emailSender;
 		private readonly IRepository<Clinic> _clinicRepository;
 		private readonly IRepository<Specialty> _specialtyRepository;
 
 		public RegisterModel(
-            UserManager<DentHubUser> userManager,
-            SignInManager<DentHubUser> signInManager,
-            ILogger<RegisterModel> logger,
-            IEmailSender emailSender,
+			UserManager<DentHubUser> userManager,
+			SignInManager<DentHubUser> signInManager,
+			ILogger<RegisterModel> logger,
+			IEmailSender emailSender,
 			IRepository<Clinic> clinicRepository,
 			IRepository<Specialty> specialtyRepository)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-            _logger = logger;
-            _emailSender = emailSender;
-			_clinicRepository = clinicRepository;		
+		{
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_logger = logger;
+			_emailSender = emailSender;
+			_clinicRepository = clinicRepository;
 			_specialtyRepository = specialtyRepository;
 		}
 
 		[BindProperty]
-        public InputModel Input { get; set; }
+		public InputModel Input { get; set; }
 
-        public string ReturnUrl { get; set; }
+		public string ReturnUrl { get; set; }
 
-        public class InputModel
-        {
+		public class InputModel
+		{
 			[Required]
 			[Display(Name = "First Name")]
 			public string FirstName { get; set; }
@@ -58,48 +60,50 @@ namespace DentHub.Web.Areas.Identity.Pages.Account
 			public string LastName { get; set; }
 
 			[Required]
-            [EmailAddress]
-            [Display(Name = "Email")]
-            public string Email { get; set; }
+			[EmailAddress]
+			[Display(Name = "Email")]
+			public string Email { get; set; }
 
-            [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Password")]
-            public string Password { get; set; }
+			[Required]
+			[StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+			[DataType(DataType.Password)]
+			[Display(Name = "Password")]
+			public string Password { get; set; }
 
-            [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            public string ConfirmPassword { get; set; }
+			[DataType(DataType.Password)]
+			[Display(Name = "Confirm password")]
+			[Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+			public string ConfirmPassword { get; set; }
 
-			//[Required]
+			[Required]
 			[Display(Name = "Clinic")]
 			public string Clinic { get; set; }
 
-			//[Required]
+			[Required]
 			[Display(Name = "Specialty")]
 			public string Specialty { get; set; }
 
 		}
 
 		public void OnGet(string returnUrl = null)
-        {
-            ReturnUrl = returnUrl;
-        }
+		{
+			ReturnUrl = returnUrl;
+			this.ViewData["ClinicList"] = _clinicRepository.All().ToList();
+			this.ViewData["SpecialtyList"] = _specialtyRepository.All().ToList();
+		}
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-        {
-            returnUrl = returnUrl ?? Url.Content("~/");
-            if (ModelState.IsValid)
-            {
-				//var clinic = this._clinicRepository
-				//				.All()
-				//				.First(c => c.Name == Input.Clinic);
+		public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+		{
+			returnUrl = returnUrl ?? Url.Content("~/");
+			if (ModelState.IsValid)
+			{
+				var clinic = this._clinicRepository
+								.All()
+								.FirstOrDefault(c => c.Id == int.Parse(Input.Clinic));
 
-				//var specialty = this._specialtyRepository
-				//				.All()
-				//				.First(s => s.Name == Input.Specialty);
+				var specialty = this._specialtyRepository
+								.All()
+								.FirstOrDefault(s => s.Id == int.Parse(Input.Specialty));
 
 				var user = new DentHubUser
 				{
@@ -107,37 +111,38 @@ namespace DentHub.Web.Areas.Identity.Pages.Account
 					Email = Input.Email,
 					FirstName = Input.FirstName,
 					LastName = Input.LastName,
-					//ClinicId = clinic.Id,
-					//SpecialtyId = specialty.Id,
+					ClinicId = clinic.Id,
+					SpecialtyId = specialty.Id,
 				};
-                var result = await _userManager.CreateAsync(user, Input.Password);
-                if (result.Succeeded)
-                {
+
+				var result = await _userManager.CreateAsync(user, Input.Password);
+				if (result.Succeeded)
+				{
 					var resultRole = await _userManager.AddToRoleAsync(user, "Dentist");
 
 					_logger.LogInformation("User created a new account with password.");
 
-                    var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { userId = user.Id, code = code },
-                        protocol: Request.Scheme);
+					var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+					var callbackUrl = Url.Page(
+						"/Account/ConfirmEmail",
+						pageHandler: null,
+						values: new { userId = user.Id, code = code },
+						protocol: Request.Scheme);
 
-                    await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                        $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+					await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+						$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
 
-                    await _signInManager.SignInAsync(user, isPersistent: false);
-                    return LocalRedirect(returnUrl);
-                }
-                foreach (var error in result.Errors)
-                {
-                    ModelState.AddModelError(string.Empty, error.Description);
-                }
-            }
+					await _signInManager.SignInAsync(user, isPersistent: false);
+					return LocalRedirect(returnUrl);
+				}
+				foreach (var error in result.Errors)
+				{
+					ModelState.AddModelError(string.Empty, error.Description);
+				}
+			}
 
-            // If we got this far, something failed, redisplay form
-            return Page();
-        }
-    }
+			// If we got this far, something failed, redisplay form
+			return Page();
+		}
+	}
 }
